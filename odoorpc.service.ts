118f7d97cd6b27ce20631ae3ed4fdb859c28850a
gavino.isoni <gavino.isoni@flosslab.com>
2018-05-17 18:02:05 +0200

    fix import throw

--- odoorpc.service.ts
+++ odoorpc.service.ts
@@ -2,6 +2,7 @@ import {Inject, Injectable} from "@angular/core"
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 import {Observable} from "rxjs/Observable";
+import 'rxjs/add/observable/throw';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/of';
